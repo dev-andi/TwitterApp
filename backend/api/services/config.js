8774aebb15f9da9804cc36e27402c86871d95aac
Devojyoti Nandi <devojyotinandi@hotmail.com>
2017-01-12 17:59:48 +0530

    Add files via upload

---
+++ backend/api/services/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    TWITTER_KEY: 'ha9mmhiXgqTqR02HvHHTUdFuu',
+    TWITTER_SECRET: 'WzyW2xh0tMIcQhaU2XPhWiG1rWR6KNNqMxFWtCT63YuKCiNFYC',
+    TWITTER_CALLBACK: 'http://localhost:8000',
+    TOKEN_SECRET: 'blah'
+}
